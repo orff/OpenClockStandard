8ef1af7d0e71b4fbaead33f9c7e3c332cd8e620b
mike h <michaelhill@gmail.com>
2021-02-07 18:25:18 -0500

    quick test for swift package to hold the models for importing and exporting

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@
 
 import PackageDescription
 
+let ocs: Target = .target(name: "OpenClockStandard")
+ocs.dependencies = []
+ocs.path = "swift"
+
 let package = Package(
     name: "OpenClockStandard",
     products: [
@@ -18,8 +22,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "OpenClockStandard",
-            dependencies: [])
+        ocs
     ]
 )
