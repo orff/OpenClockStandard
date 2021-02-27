2d2c576d55fe32c384def0c5625af96bc48dff8f
mike h <michaelhill@gmail.com>
2021-02-27 13:48:21 -0500

    updated to add path back in

--- Package.swift
+++ Package.swift
@@ -1,4 +1,11 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
+
+let ocs: Target = .target(name: "ClockStandard")
+ocs.dependencies = []
+ocs.path = "swift"
 
 let package = Package(
     name: "ClockStandard",
@@ -15,17 +22,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "ClockStandard",
-            dependencies: []),
-            path = "swift",
-        .testTarget(
-            name: "ClockStandardTests",
-            dependencies: ["ClockStandard"]),
-    ]
+    targets: [ ocs ]
 )
 
 
